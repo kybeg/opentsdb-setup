0c360778e18a71374ad9e17727a219d87b470c11
Kyrre Begnum <kyrre@begnum.no>
2014-05-23 12:44:00 +0200

    add repo

--- install.sh
+++ install.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
 
+# variables
+
+myip=$(ifconfig eth0 | grep "inet addr" | cut -f 2 -d ":" | cut -f 1 -d " ")
+
+echo $myip $(hostname) >> /etc/hosts
+
+# enter the correct hostname in /etc/hosts
+
+
+
 # update
 
 
 
 # Install oracle Java
 
-sudo add-apt-repository -y ppa:webupd8team/java
+# sudo add-apt-repository -y ppa:webupd8team/java
 
-apt-get update
+# apt-get update
 
-apt-get install -y oracle-java7-installer binutils gnuplot make
+# apt-get install -y oracle-java7-installer binutils gnuplot make
 
 
 

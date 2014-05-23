13e57293a337b063f7b7e83ae8ee7bcc17e34d40
Kyrre Begnum <kyrre@begnum.no>
2014-05-23 10:28:47 +0200

    add the installer script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+# update
+
+apt-get update
+
+# Install oracle Java
+
+apt-get install oracle-java7-install
+
+
+
+# Install HBASE
+
+
+
+# Install Hadoop
+
+
+
+# Install OpenTSDB

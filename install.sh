a141d84ce7361bba3f25708c7ae2774d47835664
Kyrre Begnum <kyrre@begnum.no>
2014-05-23 10:43:55 +0200

    add repo

--- install.sh
+++ install.sh
@@ -3,9 +3,13 @@
 
 # update
 
-apt-get update
+
 
 # Install oracle Java
+
+echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" > /etc/apt/sources.list.d/oracle.conf
+
+apt-get update
 
 apt-get install oracle-java7-install
 
